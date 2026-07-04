ee2cba39ff0e49bcaf461d27c70b22aff5a310a4
najadsaihub-eng <najadsaihub@gmail.com>
2026-07-05 02:57:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const startBtn = document.getElementById("startBtn");
+
 const welcome = document.getElementById("welcome");
 const story = document.getElementById("story");
 
 startBtn.addEventListener("click", () => {
 
-    welcome.classList.add("hidden");
-    story.classList.remove("hidden");
+    welcome.style.opacity = "0";
+
+    setTimeout(() => {
+
+        welcome.classList.add("hidden");
+
+        story.classList.remove("hidden");
+
+        story.style.opacity = "0";
+
+        setTimeout(() => {
+
+            story.style.opacity = "1";
+
+        },50);
+
+    },500);
 
 });
