70f9e212b65034727964bf1f9fe62bd1c0088c1f
najadsaihub-eng <najadsaihub@gmail.com>
2026-07-05 02:25:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const startBtn = document.getElementById("startBtn");
+const welcome = document.getElementById("welcome");
+const story = document.getElementById("story");
 
+startBtn.addEventListener("click", () => {
+
+    welcome.style.display = "none";
+    story.style.display = "flex";
+
+});
